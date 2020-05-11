c5759e6fd6734b900d2d1c429612bfc9d94fb839
shivaprasadkh <shivaprasadgowda@gmail.com>
2020-05-11 08:45:45 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,14 @@
+package org.example.day1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Maven Example program!" );
+    }
+}
